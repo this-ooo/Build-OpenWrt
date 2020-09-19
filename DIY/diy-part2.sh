e8e1a903c803a1ec06ae010698edb52ca94efc20
this-ooo <this2056@hotmail.com>
2020-09-19 13:30:42 +0800

    Add files via upload

---
+++ DIY/diy-part2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.3/g' package/base-files/files/bin/config_generate
+sed -i -e '21s/reboot/autoreboot/g' -e '26s/reboot/autoreboot/g' package/lean/luci-app-autoreboot/root/etc/init.d/autoreboot
+cp $GITHUB_WORKSPACE/DIY/wifi package/kernel/mac80211/files/lib/wifi/mac80211.sh

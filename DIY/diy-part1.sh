fe4d6528d4c2f8bda9a396d2ec5b70022ab25d3d
this-ooo <this2056@hotmail.com>
2020-12-27 16:04:56 +0800

    Update diy-part1.sh

--- DIY/diy-part1.sh
+++ DIY/diy-part1.sh
@@ -14,4 +14,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git zerotier https://github.com/mwarning/zerotier-openwrt.git' feeds.conf.default
+#sed -i '$a src-git zerotier https://github.com/mwarning/zerotier-openwrt.git' feeds.conf.default

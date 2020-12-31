3d054b2cd248d988fb61dfa5be9ba672209f070e
this-ooo <this2056@hotmail.com>
2020-12-31 19:18:40 +0800

    Update diy-part2.sh

--- DIY/diy-part2.sh
+++ DIY/diy-part2.sh
@@ -11,4 +11,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.123.3/g' package/base-files/files/bin/config_generate
-sed -i -e '21s/reboot/autoreboot/g' -e '26s/reboot/autoreboot/g' package/lean/luci-app-autoreboot/root/etc/init.d/autoreboot
+#sed -i -e '21s/reboot/autoreboot/g' -e '26s/reboot/autoreboot/g' package/lean/luci-app-autoreboot/root/etc/init.d/autoreboot

7905c7bc9083edb49e4205e6d073a151421165f8
this-ooo <this2056@hotmail.com>
2021-01-27 19:46:08 +0800

    Add files via upload

--- DIY/diy-part2.sh
+++ DIY/diy-part2.sh
@@ -5,3 +5,4 @@
 #sed -i -e '21s/reboot/autoreboot/g' -e '26s/reboot/autoreboot/g' package/lean/luci-app-autoreboot/root/etc/init.d/autoreboot
 sed -i 's/encryption=none/encryption=sae-mixed\n\t\t\tset wireless.default_radio${devidx}.key=123456789/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 cp -rf $GITHUB_WORKSPACE/DIY/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
+cp -rf $GITHUB_WORKSPACE/DIY/hosts2 package/base-files/files/etc/hosts2

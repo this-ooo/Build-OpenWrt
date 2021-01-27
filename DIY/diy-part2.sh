5060a016d989e266c53d36eb249ffc07773f7407
this-ooo <this2056@hotmail.com>
2021-01-27 16:49:05 +0800

    Update diy-part2.sh

--- DIY/diy-part2.sh
+++ DIY/diy-part2.sh
@@ -9,7 +9,6 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.123.3/g' package/base-files/files/bin/config_generate
 #sed -i -e '21s/reboot/autoreboot/g' -e '26s/reboot/autoreboot/g' package/lean/luci-app-autoreboot/root/etc/init.d/autoreboot
 sed -i 's/encryption=none/encryption=sae-mixed\n\t\t\tset wireless.default_radio${devidx}.key=123456789/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh

0e6c63c95120872b2730dd44ef93733c9ef45896
this-ooo <this2056@hotmail.com>
2021-01-30 09:47:34 +0800

    Add files via upload

--- DIY/ipv6brctl.sh
+++ DIY/ipv6brctl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ebtables -t broute -A BROUTING -p ! ipv6 -j DROP -i wan
-brctl addif br-lan wan
+ebtables -t broute -A BROUTING -p ! ipv6 -j DROP -i $(uci -q get network.wan6.ifname)
+brctl addif br-lan $(uci -q get network.wan6.ifname)
 /etc/init.d/odhcpd stop
 #/etc/init.d/odhcpd disable
 

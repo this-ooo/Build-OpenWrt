84e0ab5720832be53d21d97129792bd4b415494d
this-ooo <this2056@hotmail.com>
2021-08-13 17:46:54 +0800

    Add files via upload

--- DIY/diy-part2.sh
+++ DIY/diy-part2.sh
@@ -8,5 +8,6 @@ cp -rf $GITHUB_WORKSPACE/DIY/hosts2 package/base-files/files/etc/hosts2
 cp -rf $GITHUB_WORKSPACE/DIY/dynv6.sh package/base-files/files/etc/dynv6.sh
 cp -rf $GITHUB_WORKSPACE/DIY/dynv6cron.sh package/base-files/files/etc/dynv6cron.sh
 #cp -rf $GITHUB_WORKSPACE/DIY/ipv6brctl.sh package/base-files/files/etc/ipv6brctl.sh
-cat $GITHUB_WORKSPACE/DIY/settings package/lean/default-settings/files/zzz-default-settings > $GITHUB_WORKSPACE/DIY/zzz-default-settings
-cp -rf $GITHUB_WORKSPACE/DIY/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
+cp -rf $GITHUB_WORKSPACE/DIY/settings package/base-files/files/etc/uci-defaults/99_settings
+#cat $GITHUB_WORKSPACE/DIY/settings package/lean/default-settings/files/zzz-default-settings > $GITHUB_WORKSPACE/DIY/zzz-default-settings
+#cp -rf $GITHUB_WORKSPACE/DIY/zzz-default-settings package/lean/default-settings/files/zzz-default-settings

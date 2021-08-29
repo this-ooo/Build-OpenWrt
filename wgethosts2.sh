01056cc45f36e5b7fff60bf49c5146de4e5d2438
this-ooo <this2056@hotmail.com>
2021-08-29 19:38:47 +0800

    Add files via upload

---
+++ wgethosts2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget --no-check-certificate -O /etc/hosts2 https://raw.hellogithub.com/hosts
+#https://cdn.jsdelivr.net/gh/521xueweihan/GitHub520@main/hosts
+#https://cdn.jsdelivr.net/gh/ssrsub/ssr@master/ssrsub
+#80:FA:5B:2B:85:8A
+
+exit 0

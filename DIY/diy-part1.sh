ca1456320cf1be660ccc8dae610d17327e1a36ff
this-ooo <this2056@hotmail.com>
2022-02-03 10:34:47 +0800

    Add files via upload

--- DIY/diy-part1.sh
+++ DIY/diy-part1.sh
@@ -4,6 +4,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default

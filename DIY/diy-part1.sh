34c890e9aefd056edadd1968f497f0038079c070
this-ooo <this2056@hotmail.com>
2022-05-22 21:51:17 +0800

    Update diy-part1.sh

--- DIY/diy-part1.sh
+++ DIY/diy-part1.sh
@@ -6,5 +6,5 @@
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default

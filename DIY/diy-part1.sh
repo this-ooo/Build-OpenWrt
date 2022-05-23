b7085b392ff735a66353f7a94a47980d41c1ba3a
this-ooo <this2056@hotmail.com>
2022-05-23 20:44:42 +0800

    Update diy-part1.sh

--- DIY/diy-part1.sh
+++ DIY/diy-part1.sh
@@ -8,3 +8,4 @@ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default

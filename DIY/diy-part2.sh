78478887eb235f795934e6b9546da1d4c8d1c8e3
this-ooo <this2056@hotmail.com>
2022-08-01 21:03:46 +0800

    Add files via upload

--- DIY/diy-part2.sh
+++ DIY/diy-part2.sh
@@ -6,5 +6,3 @@ sed -i 's/encryption=none/encryption=psk-mixed\n\t\t\tset wireless.default_radio
 cp -rf $GITHUB_WORKSPACE/DIY/dynv6.sh package/base-files/files/etc/dynv6.sh
 cp -rf $GITHUB_WORKSPACE/DIY/dynv6cron.sh package/base-files/files/etc/dynv6cron.sh
 cp -rf $GITHUB_WORKSPACE/DIY/settings1 package/base-files/files/etc/uci-defaults/99_settings1
-git clone https://github.com/siwind/luci-app-wolplus.git package/luci-app-wolplus
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash

dc5d19198c43d1afdaa81b4639a3b24726aac317
this-ooo <this2056@hotmail.com>
2022-08-29 21:45:31 +0800

    Add files via upload

--- DIY/diy-part2.sh
+++ DIY/diy-part2.sh
@@ -3,5 +3,3 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 sed -i 's/encryption=none/encryption=psk-mixed\n\t\t\tset wireless.default_radio${devidx}.key=123456789/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-cp -rf $GITHUB_WORKSPACE/DIY/dynv6.sh package/base-files/files/etc/dynv6.sh
-cp -rf $GITHUB_WORKSPACE/DIY/settings1 package/base-files/files/etc/uci-defaults/99_settings1

5651ab021a8173f761344baa47c70427f593835c
this-ooo <this2056@hotmail.com>
2022-09-07 21:39:25 +0800

    Delete ipv6brctl.sh

--- DIY/ipv6brctl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ebtables -t broute -A BROUTING -p ! ipv6 -j DROP -i $(uci -q get network.wan6.ifname)
-brctl addif br-lan $(uci -q get network.wan6.ifname)
-/etc/init.d/odhcpd stop
-#/etc/init.d/odhcpd disable
-
-exit 0

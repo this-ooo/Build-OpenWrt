885c82bab7dd5ce7044edce064e3baf7ab54c1ea
this-ooo <this2056@hotmail.com>
2025-03-27 14:32:20 +0800

    Add files via upload

--- DIY/aarch64/build.sh
+++ DIY/aarch64/build.sh
@@ -6,19 +6,14 @@ if [ ! -z "$1" ];then
 fi
 
 TMPDIR=openwrt_rootfs
-OUTDIR=/opt/imgs
-IMG_NAME=openwrt-armvirt-64-default
+IMG_NAME=openwrt
 
 [ -d "$TMPDIR" ] && rm -rf "$TMPDIR"
 
-mkdir -p "$OUTDIR"  && \
-mkdir -p "$TMPDIR"  && \
-gzip -dc ${IMG_NAME}-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
-cp -f rc.local "$TMPDIR/etc/" && \
-rm -f "$TMPDIR/etc/bench.log" && \
-echo "17 3 * * * /etc/coremark.sh" >> "$TMPDIR/etc/crontabs/root" && \
+mkdir -p "$TMPDIR" && \
+gzip -dc *-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
 (cd "$TMPDIR" && tar cf ../${IMG_NAME}-rootfs-patched.tar .) && \
 docker build -t ${IMG_NAME}:${TAG} . && \
 rm -f  ${IMG_NAME}-rootfs-patched.tar && \
 rm -rf "$TMPDIR" && \
-docker save ${IMG_NAME}:${TAG} | pigz -9 > $OUTDIR/docker-img-${IMG_NAME}-${TAG}.gz
+docker save ${IMG_NAME}:${TAG}  > docker-img-${IMG_NAME}-${TAG}.tar

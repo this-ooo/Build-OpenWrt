d393fd194c2834c7ff0c89ca4fb5867f8796930e
this-ooo <this2056@hotmail.com>
2025-03-27 15:09:17 +0800

    Add files via upload

--- DIY/diy-part1.sh
+++ DIY/diy-part1.sh
@@ -6,7 +6,7 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-git clone https://github.com/siwind/luci-app-wolplus.git package/luci-app-wolplus
-
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#git clone https://github.com/siwind/luci-app-wolplus.git package/luci-app-wolplus
+#git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
--- DIY/diy-part2.sh
+++ DIY/diy-part2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
-sed -i 's/encryption=none/encryption=psk-mixed\n\t\t\tset wireless.default_radio${devidx}.key=123456789/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/encryption=none/encryption=psk-mixed\n\t\t\tset wireless.default_radio${devidx}.key=123456789/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh

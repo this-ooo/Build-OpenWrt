9144885a4c55dd9927d83decadcca48d535a12fe
this-ooo <this2056@hotmail.com>
2026-05-23 17:43:20 +0800

    Update build.sh

--- DIY/aarch64/build.sh
+++ DIY/aarch64/build.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
-TAG=latest
-if [ ! -z "$1" ];then
-	TAG=$1
+# 输出调试信息
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
+# luci-app-openclash
+PACKAGES="$PACKAGES luci-app-openclash"
+
+# 若构建openclash 则添加内核
+if echo "$PACKAGES" | grep -q "luci-app-openclash"; then
+    echo "✅ 已选择 luci-app-openclash，添加 openclash core"
+    mkdir -p files/etc/openclash/core
+    # Download clash_meta
+    META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+    wget -qO- $META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+    chmod +x files/etc/openclash/core/clash_meta
+    # Download GeoIP and GeoSite
+    wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O files/etc/openclash/GeoIP.dat
+    wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O files/etc/openclash/GeoSite.dat
+    # Download latest openclash Client
+    URL=$(curl -s https://api.github.com/repos/vernesong/OpenClash/releases/latest \
+      | grep "browser_download_url.*ipk" \
+      | head -n1 \
+      | cut -d '"' -f 4)
+    echo "OpenClash latest ipk: $URL"
+    wget "$URL" -P /home/build/immortalwrt/packages/
+else
+    echo "⚪️ 未选择 luci-app-openclash"
 fi
 
-TMPDIR=openwrt_rootfs
-IMG_NAME=openwrt
+# 构建镜像
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
+echo "$PACKAGES"
 
-[ -d "$TMPDIR" ] && rm -rf "$TMPDIR"
+make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files"
 
-mkdir -p "$TMPDIR" && \
-gzip -dc *-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
-(cd "$TMPDIR" && tar cf ../${IMG_NAME}-rootfs-patched.tar .) && \
+if [ $? -ne 0 ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
+    exit 1
+fi
 
-docker buildx create --name mybuilder --use && \
-docker buildx inspect --bootstrap && \
-docker buildx build --platform linux/arm64 -t ${IMG_NAME}:${TAG} --output=type=docker . && \
-
-#docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/this-ooo/openwrt:latest . --push && \
-
-#docker build -t ${IMG_NAME}:${TAG} . && \
-rm -f  ${IMG_NAME}-rootfs-patched.tar && \
-rm -rf "$TMPDIR" && \
-docker save ${IMG_NAME}:${TAG}  > docker-img-${IMG_NAME}-${TAG}.tar
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Build completed successfully."

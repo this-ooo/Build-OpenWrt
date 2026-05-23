de3cac14882cf4167024a8b45f68cd0b90070aad
this-ooo <this2056@hotmail.com>
2026-05-23 21:46:30 +0800

    Update build.sh

--- DIY/aarch64/build.sh
+++ DIY/aarch64/build.sh
@@ -3,7 +3,7 @@
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
 # luci-app-openclash
-PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-app-openclash -autocore -automount -kmod-amazon-ena -kmod-atlantic -kmod-bcmgenet -kmod-dwmac-imx -kmod-dwmac-rockchip -kmod-dwmac-sun8i -kmod-e1000e -kmod-fs-vfat -kmod-fsl-dpaa1-net -kmod-fsl-dpaa2-net -kmod-fsl-enetc-net -kmod-fsl-fec -kmod-gpio-pca953x -kmod-i2c-mux-pca954x -kmod-octeontx2-net -kmod-phy-aquantia -kmod-phy-broadcom -kmod-phy-marvell -kmod-phy-marvell-10g -kmod-phy-realtek -kmod-phy-smsc -kmod-vmxnet3 -ppp -ppp-mod-pppoe"
 
 # 若构建openclash 则添加内核
 if echo "$PACKAGES" | grep -q "luci-app-openclash"; then

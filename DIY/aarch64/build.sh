9a460d3802e74ea0725fdf016c9212db1ee490e5
this-ooo <this2056@hotmail.com>
2026-05-23 19:38:44 +0800

    Update build.sh

--- DIY/aarch64/build.sh
+++ DIY/aarch64/build.sh
@@ -34,19 +34,15 @@ echo "$PACKAGES"
 make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files"
 
 cd /home/build/immortalwrt/bin/targets/armsr/armv8/
-TAG=latest
-if [ ! -z "$1" ];then
-	TAG=$1
-fi
 
 TMPDIR=openwrt_rootfs
-IMG_NAME=openwrt
 
 [ -d "$TMPDIR" ] && rm -rf "$TMPDIR"
 
-mkdir -p "$TMPDIR" && \
-gzip -dc *-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
-(cd "$TMPDIR" && tar cf ../${IMG_NAME}-rootfs-patched.tar .)
+mkdir -p "$TMPDIR"
+gzip -dc *-rootfs.tar.gz | tar -xf - -C "$TMPDIR"
+cd "$TMPDIR"
+tar -cf "../openwrt-rootfs-patched.tar" .
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
